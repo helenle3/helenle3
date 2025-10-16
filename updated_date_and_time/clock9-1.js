01150bb512268ddea29cfd450d510e37c929dbf0
Helen Le <lehelen6727@gmail.com>
2025-10-16 09:44:15 -0400

    Add Clock files

---
+++ updated_date_and_time/clock9-1.js
@@ -0,0 +1,44 @@
+﻿"use strict"
+
+/*
+   New Perspectives on HTML5 and CSS3, 8th Edition
+   Tutorial 9
+   Coding Challenge 1
+
+   Clock
+   Author: Helen Le
+   Date:   11/26/2024
+
+   function getWeekday(dayNum)
+      Returns the text of the day of the week where dayNum
+      is the number of the week from 0 (Sunday) to 6 (Saturday)
+*/
+
+runClock();
+
+setInterval(runClock, 1000);
+
+function runClock(){
+   var thisDay = new Date();
+
+   var thisDate = thisDay.toLocaleDateString();
+
+   var thisDayNum = thisDay.getDay();
+
+   var thisWeekday = getWeekday(thisDayNum);
+
+   var thisTime = thisDay.toLocaleTimeString();
+
+   console.log(thisDate);
+
+   document.getElementById("date").textContent = thisDate;
+   document.getElementById("wday").textContent = thisWeekday;
+   document.getElementById("time").textContent = thisTime;
+}
+
+function getWeekday(dayNum) {
+   var wDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+   return wDays[dayNum];
+}
+
+runClock();
